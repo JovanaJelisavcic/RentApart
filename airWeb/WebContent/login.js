080449af3056e45ac51f22346a08f446c4d484db
jovana jelisavcic <jelisavcicjovana@gmail.com>
2020-08-12 13:14:47 +0200

    tried changing scripts order and versions

--- airWeb/WebContent/login.js
+++ airWeb/WebContent/login.js
@@ -9,10 +9,12 @@ $(document).ready(function() {
 			data: JSON.stringify({username: username, password: password}),
 			contentType: 'application/json',
 			success: function(user) {
+				alert( "Handler for .success() called." )
 				$('#success').text('Login Successful');
 				$("#success").show();
 			},
 			error: function(message) {
+				alert( "Handler for .error() called." )
 				$('#error').text(message);
 				$("#error").show();
 			}
