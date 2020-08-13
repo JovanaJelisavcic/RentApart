d533af2886769535d93a94efece74ffca97624de
jovana jelisavcic <jelisavcicjovana@gmail.com>
2020-08-13 20:08:25 +0200

    fixed LoginService, method login

--- airWeb/src/services/LoginService.java
+++ airWeb/src/services/LoginService.java
@@ -10,9 +10,12 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+
 
 
 
@@ -43,25 +46,28 @@ public class LoginService {
 		}
 	}
 	
-	@POST
-	@Path("/login")
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response login(User user, @Context HttpServletRequest request) {
-		UserDAO userDao = (UserDAO) ctx.getAttribute("userDAO");
-		User loggedUser = userDao.find(user.getUsername(), user.getPassword());
-		if (loggedUser != null) {
-			return Response.status(400).entity("Invalid username and/or password").build();
-		}
-		request.getSession().setAttribute("user", loggedUser);
-		return Response.status(200).build();
-	}
+	
+	
+	    @POST
+	    @Path("/login")
+	    @Consumes(MediaType.APPLICATION_JSON)
+	    public Response getMsg(@QueryParam("username") String name, 
+	             @QueryParam("password") String pass,@Context HttpServletRequest request)
+	    {
+	    	UserDAO userDao = (UserDAO) ctx.getAttribute("userDAO");
+			User loggedUser = userDao.find(name, pass);
+			if (loggedUser == null) {
+				return Response.status(400).entity("Invalid username and/or password").build();
+			}
+			request.getSession().setAttribute("user", loggedUser);
+			return Response.status(200).build();
+	    }
+	
 	
 	
 	@POST
 	@Path("/logout")
 	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
 	public void logout(@Context HttpServletRequest request) {
 		request.getSession().invalidate();
 	}
