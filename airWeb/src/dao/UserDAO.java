462e7a76c08011884fdd51b160b3dc8afc2f61f8
jovana jelisavcic <jelisavcicjovana@gmail.com>
2020-08-18 23:05:34 +0200

    saving new user to a json file, tested from code

--- airWeb/src/dao/UserDAO.java
+++ airWeb/src/dao/UserDAO.java
@@ -8,11 +8,10 @@ import org.json.simple.parser.JSONParser;
 import java.io.FileReader;
 
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Collection;
 import java.util.HashMap;
-
-
-
 import java.util.Map;
 
 import beans.User;
@@ -22,17 +21,18 @@ import beans.User;
 
 public class UserDAO {
 	
-	
+	private String ctx;
 	
 	private Map<String, User> users = new HashMap<>();
-	
 
+	static private FileWriter file;
 	
 	public UserDAO() {
 		
 	}
 	
 	public UserDAO(String contextPath) {
+		ctx=contextPath;
 		loadUsers(contextPath);
 	}
 	
@@ -56,14 +56,14 @@ public class UserDAO {
 	}
 	
 	public  boolean saveUser(User user) 
-    {
-        //ovde ce biti mozda dodavanje u txt fajl i plus u users collection
-		//takodje sve hendlovanje treba da ide u posebne fje
-       //user.writeInFile();
-       users.put(user.getUsername(), user);
-       if(users.containsKey(user.getUsername())){
-       return true;
-       }else return false;
+    {      		   
+    	   users.put(user.getUsername(), user);
+    	   if(!makeRoomForNoob(user)){
+    		   return false;
+    		  
+    	   }else return true;
+    	   
+
     }
 	
 	public Collection<User> findAll() {
@@ -78,6 +78,7 @@ public class UserDAO {
 	 
 				JSONArray usersList = (JSONArray) obj;
 				usersList.forEach( user -> parseUserObject( (JSONObject) user ) );
+				
 	
 			} catch (Exception e) {
 				e.printStackTrace();
@@ -97,10 +98,57 @@ public class UserDAO {
 	        users.put(username, new User(firstName, lastName, sex, username, password, role));
 	    }
 	 
+	   private boolean makeRoomForNoob(User user){
+		   JSONParser parser = new JSONParser();
+		   JSONArray usersList = new JSONArray();
+			try {
+				Object obj = parser.parse(new FileReader(ctx + "/adminsinfo.json"));
 	 
-	 
-	
-	
+				 usersList = (JSONArray) obj;				 
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+				return false;
+			}
+			if(usersList!=null){
+		   writeNoob(usersList, user);
+		   return true;
+			}else return false;		   
+	    }
+
+	@SuppressWarnings({ "unchecked" })
+	private void writeNoob(JSONArray usersList, User user) {
+		// TODO Auto-generated method stub
+		//final way object should look
+        JSONObject obj = new JSONObject();
+        //here I'll put the info about the user
+        JSONObject userOb=new JSONObject();
+
+        userOb.put("username", user.getUsername());
+        userOb.put("password", user.getPassword());
+        userOb.put("firstName", user.getFirstName());
+        userOb.put("lastName", user.getLastName());
+        userOb.put("sex", user.getSex());
+        userOb.put("role", user.getRole());
+        
+        obj.put("user", userOb);
+        
+        usersList.add(obj);
+        
+        try {
+
+        	file = new FileWriter(ctx+"\\adminsinfo.json", false);
+            file.write(usersList.toJSONString());
+            
+ 
+        } catch (IOException e) {
+            e.printStackTrace();
+ 
+        } finally {
+            if (file != null) try { file.close(); } catch (IOException ignore) {}
+        }
+	}
+	 	
 	
 }
 
--- airWeb/src/testUser.java
+++ airWeb/src/testUser.java
@@ -11,8 +11,8 @@ public class testUser {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		UserDAO users = new UserDAO("C:\\Users\\jelis\\git\\web2020\\airWeb\\src");
-		User u = users.find("administer", "58fk4d");
-		User j = new User("jovana", "jelisavcic", "female", "jovanaje", "nekrakta", "host");
+		User j = new User("nina", "suknjaja", "female", "ninasfoodart", "slikajte", "guest");
+		users.saveUser(j);
 		
 	}
 
